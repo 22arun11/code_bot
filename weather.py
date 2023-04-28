d2ecc60c1e0a27a9792a0a559aaf0748c6275106
Kamalesh-L <93050548+Kamalesh-L@users.noreply.github.com>
2023-04-28 10:48:48 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -148,7 +148,7 @@ try:
                 '<b>HUMIDITY</b>'],
                 line_color='black', fill_color='royalblue',  font=dict(color='white', size=14),height=32),
     cells=dict(values=[dates,maxtemp,mintemp,rain,cloud,humd],
-    line_color='black',fill_color=['royalblue',['black', 'red']*7], font_size=14,height=32
+    line_color='black',fill_color=['royalblue',['black', 'crimson']*7], font_size=14,height=32
         ))])
 
     table1.update_layout(margin=dict(l=10,r=10,b=10,t=10),height=328)
@@ -157,7 +157,7 @@ try:
     table2=go.Figure(data=[go.Table(columnwidth=[1,2,1,1,1,1],header=dict(values=['<b>DATES</b>','<b>WEATHER CONDITION</b>','<b>WIND SPEED</b>','<b>PRESSURE<br>(in hPa)</b>','<b>SUNRISE<br>(in UTC)</b>','<b>SUNSET<br>(in UTC)</b>']
                 ,line_color='black', fill_color='royalblue',  font=dict(color='white', size=14),height=36),
     cells=dict(values=[dates,desc,wspeed,pres,sunrise,sunset],
-    line_color='black',fill_color=['royalblue',['black', 'red']*7], font_size=14,height=36))])
+    line_color='black',fill_color=['royalblue',['black', 'crimson']*7], font_size=14,height=36))])
     
     table2.update_layout(margin=dict(l=10,r=10,b=10,t=10),height=360)
     st.write(table2)
