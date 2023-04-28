105d4c20affc9c8aba9976123496dc4dc294a6ed
Kamalesh-L <93050548+Kamalesh-L@users.noreply.github.com>
2023-04-28 23:37:34 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -220,7 +220,7 @@ with st.expander("1.CHAT BOT"):
 
             for i in range(len(st.session_state["generated"]) - 1, -1, -1):
                 message(st.session_state["generated"][i], key=str(i))
-#                 message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
+                message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
 
     
 
